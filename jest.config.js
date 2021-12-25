4f3e7dc776d4fc7b28a18938b5c0080f2dc50cc8
João Vitor <jsoareez@gmail.com>
2021-12-25 03:23:40 -0300

    refactor: fix jest config

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
-export default {
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
   roots: ['<rootDir>/src'],
-  collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
-  testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
