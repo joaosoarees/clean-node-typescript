3428852fee4648640015b18686a667c859efa1ea
João Vitor <jsoareez@gmail.com>
2022-01-02 11:36:00 -0300

    Revert "refactor: move mongodb methods to a helper"
    This reverts commit ebb3d563059c9f24c8825dda5d36c9aaefcc36a4.

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: '@shelf/jest-mongodb',
+  preset: ['ts-jest', '@shelf/jest-mongodb'],
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
