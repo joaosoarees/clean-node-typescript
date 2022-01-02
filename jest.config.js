d78596398ec859f17035406b3356af28b921df25
João Vitor <jsoareez@gmail.com>
2022-01-02 11:37:23 -0300

    revert

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: ['ts-jest', '@shelf/jest-mongodb'],
+  preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
